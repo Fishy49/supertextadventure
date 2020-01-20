94f32701cf456b73540fc6af1f21042f5c6ae1af
Richard Cagle <contact@richardleecagle.com>
2020-01-19 21:30:03 -0600

    update styling and game message format

--- app/controllers/game_messages_controller.rb
+++ app/controllers/game_messages_controller.rb
@@ -1,6 +1,10 @@
 class GameMessagesController < ApplicationController
   def create
+    previous_message_username = GameMessage.where(game_id: game_message_params[:game_id]).last.user.username
     @game_message = GameMessage.create game_message_params.merge(user_id: current_user.id)
+
+
+    @show_username = @game_message.user.username != previous_message_username
 
     message_script_template = render_to_string template: 'game_messages/create.js.erb', layout: false
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
@@ -66,4 +64,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #--
+
+  config.action_mailer.smtp_settings = { port: 1025 }
 end

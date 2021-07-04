054542ec5d5d86a6a2025552a4c6411124d49362
Richard Cagle <Rick.Cagle@Fabricut.com>
2021-07-03 23:50:38 -0500

    add more styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :logged_in?
 
   def current_user
     if session[:user_id]
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     else
       @current_user = nil
     end
+  end
+
+  def logged_in?
+    current_user.present?
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:username])
+    user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"

41e1f1c28b54a518382577edbbe9d7315db9fe1f
Richard Cagle <Rick.Cagle@Fabricut.com>
2022-01-12 00:03:19 -0600

    setup associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Game < ApplicationRecord
+  belongs_to :host, class_name: "User",
+                    foreign_key: :created_by,
+                    primary_key: :id,
+                    dependent: :destroy,
+                    inverse_of: :hosted_games,
+                    optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
+
+  has_many :hosted_games, class_name: "Game", foreign_key: :created_by, dependent: :destroy, inverse_of: :host
 end

811583986b650e3520ea71b8567957889b5efb03
Richard Cagle <rick.cagle@hey.com>
2023-01-29 16:30:50 -0600

    put functions in main file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,40 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "src/beep.js"
-import "src/place_caret_at_end"
+
+// https://stackoverflow.com/a/49077414
+window.beep = function(vol, freq, duration){
+  if(!window.audio_context){
+    window.audio_context = new AudioContext();
+  }
+  
+  let v = window.audio_context.createOscillator()
+  let u = window.audio_context.createGain()
+
+  v.connect(u)
+  v.frequency.value=freq
+  v.type="sawtooth"
+  u.connect(window.audio_context.destination)
+  u.gain.value=vol*0.01
+  v.start(window.audio_context.currentTime)
+  v.stop(window.audio_context.currentTime+duration*0.001)
+}
+
+// https://stackoverflow.com/a/4238971
+window.placeCaretAtEnd = function(el) {
+  if (typeof window.getSelection != "undefined"
+          && typeof document.createRange != "undefined") {
+    let range = document.createRange();
+    range.selectNodeContents(el);
+    range.collapse(false);
+    let sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+  } else if (typeof document.body.createTextRange != "undefined") {
+    let textRange = document.body.createTextRange();
+    textRange.moveToElementText(el);
+    textRange.collapse(false);
+    textRange.select();
+  }
+}
+

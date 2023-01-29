b7cc865afe7113a73385f617b89a35ffc8a66190
Richard Cagle <rick.cagle@hey.com>
2023-01-29 16:27:02 -0600

    trying something else

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./src/beep.js"
-import "./src/place_caret_at_end"
+import "src/beep.js"
+import "src/place_caret_at_end"
